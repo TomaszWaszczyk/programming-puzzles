8327c710ac172692fb3e06d15142d64ff3daacb3
Tomasz Waszczyk <tomasz@waszczyk.com>
2019-03-22 08:17:26 +0100

    Update its-friday.js

--- javascript/fridays-puzzles/its-friday.js
+++ javascript/fridays-puzzles/its-friday.js
@@ -1,4 +1,7 @@
-//1
+// 0
+-!~ -~1 
+
+// 1
 function cb(){
   console.log('Processed in next iteration');
 }
@@ -8,3 +11,40 @@ console.log('Processed in the first iteration');
 
 // 2
 console.log.call.apply(a => a, [1,2])
+
+// 3 - What will happen and how to fix it. ;-)
+const arr = ['a', 'b', 'c', 'd'];
+for (var i = 0; i < arr.length; i++) {
+  setTimeout(function() {
+    console.log(`Index : ${i}, element: ${arr[i]}`)
+  }, 1000);
+}
+
+// 4
+parseInt('101',2) << 2
+
+
+// 5
+console.log(typeof typeof 1)
+
+
+// 6
+'ababab'.split('').map((c,i,a) => ~c.indexOf(a[-~1] -- ~1).filter((c,i,a) => a[-~i] == c)
+
+                       
+// 7
+(! + [] + [] + ([] + [] === "") + (Math.max() === -Infinity )).length === 12
+
+                       
+// 8
+let output = (function(x) {
+  delete x;
+  return x;
+})(0);
+
+
+// 9
+Array(16).join( 'hero'-1) + "Batman";
+
+// 10
+
