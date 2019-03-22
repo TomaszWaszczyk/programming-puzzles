0da074f9dc06c61547152e855f04f2b4c2876982
Tomasz Waszczyk <tomasz@waszczyk.com>
2019-03-22 07:58:05 +0100

    Update its-friday.js

--- javascript/fridays-puzzles/its-friday.js
+++ javascript/fridays-puzzles/its-friday.js
@@ -1,5 +1,10 @@
+//1
 function cb(){
   console.log('Processed in next iteration');
 }
 process.nextTick(cb);
 console.log('Processed in the first iteration');
+
+
+// 2
+console.log.call.apply(a => a, [1,2])
