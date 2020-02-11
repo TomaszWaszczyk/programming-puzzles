b1348606047dec8fc1710cf55891383ae19e65c0
Tomasz Waszczyk <tomasz@waszczyk.com>
2020-02-11 14:41:07 +0100

    Update Puzzle1.java

--- java/Puzzle1.java
+++ java/Puzzle1.java
@@ -1,3 +1,5 @@
+// czym jest monada, jak działa hashmap (szczegóły implementacji), co się dzieje jak ktoś wykonuje połączenie http (coś o http / tcp / dns), pytania o wieloątkowość (monitor w Javie, Future w Scali), pytania o różnice w relacyjnych i niereacyjnych bazach danych, o backpressure w HTTP (zwracanie kodu 409), o batchowanie w Kafce
+
 package com.waszczyk
 
 import java.util.*;
